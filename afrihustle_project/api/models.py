084aab9d727db51731d86b081dcc9a1c07f20577
Rejoice Oyediran <rejoiceoyediran9@gmail.com>
2025-04-15 19:22:06 +0100

    made changes to my model

--- afrihustle_project/api/models.py
+++ afrihustle_project/api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here. The class here is the table in the database and the characters under represent the columns in the table.
 class User(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid4)
     full_name = models.CharField(max_length=100)
 
 
--- afrihustle_project/api/views.py
+++ afrihustle_project/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from models import User
+
 from .serializer import UserSerializer
 # We import these libraries so we can handle the logic for the api endpoints
 
-@api_view(('GET'))
+@api_view(['GET'])
 def get_user(request):
-    return Response(UserSerializer({"name": "Lima", "age": 23}))
+    return Response(UserSerializer({"full_name": "Lima", "age": 23}).data)
