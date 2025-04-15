abb57dc3e42ecd68afb7f502ef42ab770728186c
Rejoice Oyediran <rejoiceoyediran9@gmail.com>
2025-04-15 19:04:41 +0100

    testing out the api endpoints using serializer on fake data

--- afrihustle_project/api/models.py
+++ afrihustle_project/api/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. The class here is the table in the database and the characters under represent the columns in the table.
 class User(models.Model):
-    age = models.IntegerField()
-    name = models.CharField(max_length=100)
-
+    id = models.UUIDField()
+    full_name = models.CharField(max_length=100)
 
 
 
--- afrihustle_project/api/views.py
+++ afrihustle_project/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from models import User
+from .serializer import UserSerializer
+# We import these libraries so we can handle the logic for the api endpoints
 
-# Create your views here.
+@api_view(('GET'))
+def get_user(request):
+    return Response(UserSerializer({"name": "Lima", "age": 23}))
