4f4a67a30f1ee6c0c7b135be2b2401d2122225da
Rejoice Oyediran <rejoiceoyediran9@gmail.com>
2025-04-16 16:05:18 +0100

    adding functionality to the model using serializers

--- afrihustle_project/api/models.py
+++ afrihustle_project/api/models.py
@@ -3,8 +3,12 @@ from uuid import uuid4
 
 # Create your models here. The class here is the table in the database and the characters under represent the columns in the table.
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4())
     full_name = models.CharField(max_length=100)
+    age = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return str(self.id) 
 
 
 
--- afrihustle_project/api/views.py
+++ afrihustle_project/api/views.py
@@ -8,3 +8,11 @@ from .serializer import UserSerializer
 @api_view(['GET'])
 def get_user(request):
     return Response(UserSerializer({"full_name": "Lima", "age": 23}).data)
+
+@api_view(['POST'])    # Functionality of adding user
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
