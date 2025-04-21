8468c96ff6e8e20245626868048c1dad933d20a7
Rejoice Oyediran <rejoiceoyediran9@gmail.com>
2025-04-21 17:26:25 +0100

    integrating users table with api

--- afrihustle_project/api/models.py
+++ afrihustle_project/api/models.py
@@ -3,12 +3,24 @@ from uuid import uuid4
 
 # Create your models here. The class here is the table in the database and the characters under represent the columns in the table.
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4())
-    full_name = models.CharField(max_length=100)
-    age = models.IntegerField(blank=False)
+    class user_role(models.TextChoices):
+        Worker = "Worker"
+        Employer = "Employer"
+        Admin = "Admin"
 
-    def __str__(self):
-        return str(self.id) 
+
+    id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+    full_name = models.CharField(max_length=250, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    password = models.TextField(blank=False)
+    phone_number = models.TextField(blank=False, max_length=20)
+    role = models.CharField(max_length=8, choices=user_role.choices)
+    location = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
 
 
 
